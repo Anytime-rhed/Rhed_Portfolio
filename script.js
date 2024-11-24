fe7ff990c0022adb361c93840970acf837d5dac6
Anytime-rhed <salacroldan@gmail.com>
2024-11-24 23:05:05 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+/* ===== Typing Animation ===== */
+var typed = new Typed(".typing",{
+    strings:["","Student-Leader", "Artist", "Carat"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
